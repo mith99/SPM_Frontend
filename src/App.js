f9cce3b5a8532a17e19b0e21a770e0d582eb16f9
VixenSv <queenvixen0@gmail.com>
2021-08-21 00:29:19 +0530

    intial push to it19167992 branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import createCategory from './IT19167992/Components/Category/add-category';
+import categoryList from './IT19167992/Components/Category/category-list';
+import createMeal from './IT19167992/Components/Meal/add-meal';
+import ViewMealsBackPanel from "./IT19167992/Components/Meal/meals-list";
+import EditAndDeleteCategory from './IT19167992/Components/Category/edit-delete-category';
+import EditMeal from './IT19167992/Components/Meal/edit-delete-meal';
 function App() {
   return (
    <div>
       <Router>
         <Switch>
-          <Route path="/" component={}></Route>
+        
+          <Route path="/add-category" component={createCategory} />
+          <Route path="/category-list" component={categoryList} />
+          <Route path="/add-meal" component={createMeal} />
+          <Route path="/meal-list" component={ViewMealsBackPanel} />
+          <Route path="/edit-delete-category/:id" component={EditAndDeleteCategory} />
+          <Route path="/edit-delete-meal/:id" component={EditMeal} />
         </Switch>
       </Router>
     </div>
