6115fca3fd4dcf0f7bd89f4d2fcd4eea9db95b81
vikum <vchamindu@gmail.com>
2021-08-24 23:50:47 +0530

    added app .js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ViewOrdersBackPanel from "./IT19134536/pages/Order-page-backpannel/pages/view-orders"
+import ViewOrderSingleBackPanel from "./IT19134536/pages/Order-page-backpannel/pages/view-single-order"
+import ViewFeedbackPanel from "./IT19134536/pages/Feedback-pages/pages/view-feedback"
+import Navbar from "./sideNavBar";
 
 function App() {
   return (
-   <div>
+    <div>
+      <Navbar />
       <Router>
         <Switch>
-          <Route path="/" component={}></Route>
+          <Route path="/order/view-orders-list" component={ViewOrdersBackPanel}></Route>
+          <Route path="/feedback/view-feedbacks-list" component={ViewFeedbackPanel}></Route>
+          <Route path="/order/view-single-order" component={ViewOrderSingleBackPanel}></Route>
         </Switch>
       </Router>
     </div>
